434bc0dfb0ec35f23a2109b658f8abaf5a84ab54
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-17 11:23:32 -0700

    create product route and fix all styling issues

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,23 +1,25 @@
 import Link from 'next/link';
 import React from 'react'
 import { FaCartShopping } from "react-icons/fa6";
+import Logo from "../component/Img/HLogo.png"
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <div>
         <header className="text-gray-600 bg-slate-100 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">Tailblocks</span>
-    </a>
+  <div className=" mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center md:mr-60 ">
+    <Link href={'/'}className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Image src={Logo} height={10} width={100}  />
+     
+      <span className="ml-1 text-xl">FashionWear</span>
+    
+    </Link>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap cursor-pointer text-gray-900 font-medium items-center text-base justify-center">
-      <Link href={'/tshirt'} className="mr-5 hover:text-gray-900">Tshirts</Link>
-      <Link href={'/mugs'}className="mr-5 hover:text-gray-900">Mugs</Link>
-      <Link href={'/hoodies'} className="mr-5 hover:text-gray-900">Hoodies</Link>
-      <Link href={'/stickers'} className="mr-5 hover:text-gray-900">Stickers</Link>
+      <Link href={'/tshirt'} className="mr-4 hover:text-gray-900">Tshirts</Link>
+      <Link href={'/mugs'}className="mr-4 hover:text-gray-900">Mugs</Link>
+      <Link href={'/hoodies'} className="mr-4 hover:text-gray-900">Hoodies</Link>
+      <Link href={'/stickers'} className="mr-4 hover:text-gray-900">Stickers</Link>
     </nav>
     
     <FaCartShopping  className='text-xl absolute right-0 mx-4 top-6' />
--- src/pages/tshirt.js
+++ src/pages/tshirt.js
@@ -2,7 +2,108 @@ import React from 'react'
 
 const Tshirt = () => {
   return (
-    <div>Tshirt</div>
+    <div>
+
+    <section className="text-gray-600 body-font">
+        <div class="flex py-14 flex-col text-center w-full">
+            
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Welcome to Our Ecommerce Store</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+        </div>
+        <div className="container px-5 py-7 mx-auto">
+            <div className="flex flex-wrap -m-4">
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md  ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                        <p className="mt-1">$21.15</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                        <p className="mt-1">$12.00</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                        <p className="mt-1">$18.40</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                        <p className="mt-1">$21.15</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                        <p className="mt-1">$12.00</p>
+                    </div>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                        <p className="mt-1">$18.40</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
   )
 }
 
