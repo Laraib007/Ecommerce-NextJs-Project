41dcdd1659ccebf0e4d4795260d069eb39d71a19
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-17 08:49:24 -0700

    using cart png

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
@@ -17,11 +18,8 @@ const Navbar = () => {
       <a className="mr-5 hover:text-gray-900">Third Link</a>
       <a className="mr-5 hover:text-gray-900">Fourth Link</a>
     </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+    <FaCartShopping className='text-xl' />
+   
   </div>
 </header>
     </div>
