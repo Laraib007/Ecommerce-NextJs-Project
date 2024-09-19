6e29bb82391b1c78edd05af461f1651d0ba3706f
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-19 09:05:21 -0700

    cart fixed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -22,8 +22,8 @@ const Navbar = () => {
       <Link href={'/hoodies'} className="mr-4 hover:text-gray-900">Hoodies</Link>
       <Link href={'/stickers'} className="mr-4 hover:text-gray-900">Stickers</Link>
     </nav>
-    <FaCartShopping  className='text-xl absolute right-0 mx-4 top-6 translate-x-full transform transition-transform' />
-    <div className='absolute top-0 right-0 p-10 bg-pink-300'>
+    <FaCartShopping  className='text-xl absolute right-0 mx-4 top-6' />
+    <div className='absolute top-0 right-0 p-10 bg-pink-300  transform transition-transform translate-x-full '>
    <div className='text-lg font-bold'>This is Store Cart</div> 
     <div><span>T-shirt - Stylish tshirt</span></div>
     <div className='absolute top-2 right-4'><MdCancel className='text-xl text-pink-600' /></div>
