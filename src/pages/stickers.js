84ee553db7686dd3c6aea64177c323f7a82a154f
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-19 12:11:32 -0700

    setting stickers page

--- src/pages/stickers.js
+++ src/pages/stickers.js
@@ -11,10 +11,10 @@ const Sticker = () => {
         </div>
         <div className="container px-5 py-7 mx-auto">
             <div className="flex flex-wrap m-4 justify-center">
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
                       
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
                     </a>
                     <div className="mt-4">
                         <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -22,10 +22,50 @@ const Sticker = () => {
                         <p className="mt-1">$16.00</p>
                     </div>
                 </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
                       
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
+                      
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
+                      
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
+                      
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
+                    </a>
+                    <div className="mt-4">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                        <p className="mt-1">$16.00</p>
+                    </div>
+                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+                    <a className=" relative rounded overflow-hidden contents">
+                      
+          <img alt="ecommerce" className="object-cover object-center h-52 block " src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
                     </a>
                     <div className="mt-4">
                         <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -33,84 +73,6 @@ const Sticker = () => {
                         <p className="mt-1">$16.00</p>
                     </div>
                 </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full h-2/3 shadow-md m-4">
-                    <a className="block relative rounded overflow-hidden">
-                      
-          <img alt="ecommerce" className="object-cover w-56 object-center ml-6 h-full block" src="https://m.media-amazon.com/images/I/819YJySSaeL._AC_SX679_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
-
             </div>
         </div>
     </section>
