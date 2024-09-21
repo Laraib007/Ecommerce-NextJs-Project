5606a1e42e0ef65682567a01b309daccea16d2a1
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-21 04:44:54 -0700

    creating zipcode checker

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -85,6 +85,10 @@ const Slugs =()=> {
             </svg>
           </button>
         </div>
+        <div className='flex'>
+        <input></input>
+        <button class="flex ml-2 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Check</button>
+        </div>
       </div>
     </div>
   </div>
