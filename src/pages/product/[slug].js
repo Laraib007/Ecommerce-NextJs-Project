c14480b964f7fa9b8e2a5cd778657eda65e3f322
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-21 04:40:15 -0700

    creating buy now button with add to cart button

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -7,7 +7,7 @@ const Slugs =()=> {
   return <>
   <section class="text-gray-600 body-font overflow-hidden">
   <div class="container  px-5 py-24 mx-auto">
-    <div class="lg:w-4/5 mr-8 mx-auto flex flex-wrap">
+    <div class="lg:w-4/5 md:mr-8 mx-auto flex flex-wrap">
     <Image alt="ecommerce" width={380} height={100} class=" border-2 object-cover object-center rounded " loader={() => src} src={src}></Image>
       {/* <img alt="ecommerce" class=" border-2 lg:w-1/2 w-full lg:h-auto h-74 object-cover object-center rounded " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg" /> */}
       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -78,6 +78,7 @@ const Slugs =()=> {
         <div class="flex">
           <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
           <button class="flex ml-3 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Add to Cart</button>
+          <button class="flex ml-2 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Buy Now</button>
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
