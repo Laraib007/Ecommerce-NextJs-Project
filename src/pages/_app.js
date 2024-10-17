648958a088b04dfa9ffc046a11754039523c37eb
Laraib007 <lrbkhan01@gmail.com>
2024-10-17 06:51:57 +0500

    creating addToCart

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,19 @@
 import Footer from "@/component/Footer";
 import Navbar from "@/component/Navbar";
 import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
   const [cart, setcart] = useState({})
   const [subTotal, setsubTotal] = useState(0)
 
 const addToCart =(itemCode, price, qty, size, varrient, color)=>{
-let myCart = cart;
-if(myCart in itemCode)
-
-
+let newCart = cart;
+if(itemCode in cart){
+  newCart[itemCode].qty = newCart[itemCode].qty + qty}
+  else (
+    newCart[itemCode] = {qty: 1, price, size, varrient, color }
+  )
 }
 
   return (
