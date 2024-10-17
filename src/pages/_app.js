37015ffae3ec4c858d185c980a48a3b43c80c18f
Laraib007 <lrbkhan01@gmail.com>
2024-10-17 06:45:38 +0500

    creating addToCart func

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,16 @@ import Navbar from "@/component/Navbar";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const [cart, setcart] = useState({})
+  const [subTotal, setsubTotal] = useState(0)
+
+const addToCart =(itemCode, price, qty, size, varrient, color)=>{
+let myCart = cart;
+if(myCart in itemCode)
+
+
+}
+
   return (
     <>
     <Navbar />
