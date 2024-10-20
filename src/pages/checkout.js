95850b1a11ed8d1839626d4d69cab2134f93cc17
Laraib007 <lrbkhan01@gmail.com>
2024-10-20 16:40:26 +0500

    fixing

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,14 +5,14 @@ const Checkout = () => {
     <div className='container'>
       
       <h1 className='m-2 font-bold text-center'>CHECKOUT</h1>
-     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <div>
+     <div class="mt-10 row ">
+      <div className='w-96'>
         <label for="email" class=" text-sm font-medium  text-gray-900">Email address</label>
         <div class="mt-2">
           <input id="email" name="email" type="email" autocomplete="email" required class=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
-      <div>
+      <div className='w-96'>
         <label for="email" class=" text-sm font-medium  text-gray-900">Email address</label>
         <div class="mt-2">
           <input id="email" name="email" type="email" autocomplete="email" required class=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
