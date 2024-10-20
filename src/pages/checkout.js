1d033dd6bdeea09e02c4bfb4f6a5e311e91c377b
Laraib007 <lrbkhan01@gmail.com>
2024-10-20 16:55:55 +0500

    filing details

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,18 +5,25 @@ const Checkout = () => {
     <div className='container'>
       
       <h1 className='m-2 font-bold text-center'>CHECKOUT</h1>
-     <div class="mt-10 row ">
-      <div className='w-96'>
-        <label for="email" class=" text-sm font-medium  text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-        </div>
+     <div className="mt-10 flex container justify-center ">
+  
+      <div className='w-96 m-3 '>
+        <label for="name" className=" text-sm font-medium ml-1 text-gray-900">Full Name</label>
+          <input id="name" name="name" type="text" autocomplete="name" required className=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
-      <div className='w-96'>
-        <label for="email" class=" text-sm font-medium  text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-        </div>
+      <div className='w-96 m-3 '>
+        <label for="email" className=" text-sm font-medium ml-1 text-gray-900">Email Address</label>
+          <input id="email" name="email" type="email" autocomplete="email" required className=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      </div>
+      </div>
+      <div className="mt-2 flex container justify-center ">
+      <div className='w-96 m-3 '>
+        <label for="number" className=" text-sm font-medium ml-1 text-gray-900">Cell No</label>
+          <input id="number" name="number" type="number" autocomplete="number" required className=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      </div>
+      <div className='w-96 m-3 '>
+        <label for="name" className=" text-sm font-medium ml-1 text-gray-900">Full Name</label>
+          <input id="name" name="name" type="text" autocomplete="name" required className=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
       </div>
     </div>
