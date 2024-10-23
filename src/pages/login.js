c2ba3b3212c37b0fad061bf006bfeeb3a935373d
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 22:26:21 +0500

    fixing forget passwod page

--- src/pages/login.js
+++ src/pages/login.js
@@ -34,18 +34,18 @@ const Login = () => {
           <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
         <div class="text-sm text-center">
-            <a href="#" class="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</a>
+            <Link href="/forgotpass" class="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</Link>
           </div>
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Sign in</button>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Get Code</button>
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
-      Not a member?
-      <Link href="signup" class="font-semibold leading-6 text-pink-600 hover:text-pink-500">Signup Now!</Link>
+      Already a member?
+      <Link href="login" class="font-semibold leading-6 text-pink-600 hover:text-pink-500">Login Now!</Link>
     </p>
   </div>
 </div>
