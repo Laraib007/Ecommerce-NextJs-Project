f478af5797577347626edd842b73425cdaad373c
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 22:28:28 +0500

    fixed all issues Alhamdulilla

--- src/pages/login.js
+++ src/pages/login.js
@@ -39,13 +39,13 @@ const Login = () => {
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Get Code</button>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Sign in</button>
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
-      Already a member?
-      <Link href="login" class="font-semibold leading-6 text-pink-600 hover:text-pink-500">Login Now!</Link>
+      Not a member?
+      <Link href="signup" class="font-semibold leading-6 text-pink-600 hover:text-pink-500">Signup Now!</Link>
     </p>
   </div>
 </div>
