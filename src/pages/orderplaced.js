67b896d088a0afdb15ae56acdeb1d7531125bacd
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 19:23:28 +0500

    setting rows

--- src/pages/orderplaced.js
+++ src/pages/orderplaced.js
@@ -13,8 +13,8 @@ const orderplaced = () => {
         <p class="leading-relaxed mb-4">Best Quality - Premium Desings - Stylish Look - Better Experience</p>
         <div class="flex mb-4">
           <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1">Description</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Qty</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Price</a>
+          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1">Qty</a>
+          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1">Price</a>
         </div>
         <div class="flex border-t border-gray-200 py-2">
           <span class="text-gray-500">Fashion Wear Black T-Shirt(Xl)</span>
@@ -26,12 +26,12 @@ const orderplaced = () => {
           <span class="ml-auto text-gray-900">02</span>
           <span class="ml-auto text-gray-900">Rs.499</span>
         </div>
-        <div class="flex border-t mb-3 border-gray-200 py-2">
+        <div class="flex border-t border-gray-200 py-2">
           <span class="text-gray-500">Fashion Wear Black T-Shirt(Xl)</span>
           <span class="ml-auto text-gray-900">02</span>
           <span class="ml-auto text-gray-900">Rs.499</span>
         </div>
-        <div class="flex">
+        <div class="flex mt-5">
           <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
           <button class="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Button</button>
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
