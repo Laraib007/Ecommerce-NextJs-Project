1602c27b84ccce0056bce309f9d17c721f261fef
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 21:13:27 +0500

    fixing

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -43,9 +43,9 @@ const Checkout = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
   </div>
   {/* <-------CART ITEMS------> */}
   </div>
-    <div className=' bg-pink-300 w-2/3 center  px-1'>
+    <div className=' bg-pink-300 text-center w-full items-center mx-auto my-auto justify-center px-1'>
    <h1 className='text-lg text-center text-gray-800 font-bold m-2'>Review Your Cart Items</h1> 
-    <ol className='font-semibold list-decimal ml-80'>
+    <ol className='font-semibold text-center list-decimal '>
       {Object.keys(cart).length == 0 && <div className='ml-8'>Your Cart is Empty!</div>}
       {Object.keys(cart).map((k)=>{return <li key={k}>
         <div className='item flex '>
