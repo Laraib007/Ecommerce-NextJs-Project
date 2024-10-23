2c801097540f7400db52b5ed259f128314219c24
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 19:34:00 +0500

    setting all

--- src/pages/orderplaced.js
+++ src/pages/orderplaced.js
@@ -8,13 +8,13 @@ const orderplaced = () => {
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
       <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
         <h2 class="text-sm title-font text-gray-500 tracking-widest">FASHION WEAR</h2>
-        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">FASHION FOR ALL</h1>
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">ORDER ID #312478</h1>
        
         <p class="leading-relaxed mb-4">Best Quality - Premium Desings - Stylish Look - Better Experience</p>
         <div class="flex text-center mb-4">
-          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1 mr-20">Description</a>
-          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1 mr-4">Qty</a>
-          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1">Price</a>
+          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1 mr-40">Description</a>
+          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1 mr-6">Qty</a>
+          <a class="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1 ml-8">Price</a>
         </div>
         <div class="flex border-t border-gray-200 py-2">
           <span class="text-gray-500">Fashion Wear Black T-Shirt(Xl)</span>
@@ -32,13 +32,8 @@ const orderplaced = () => {
           <span class="ml-auto text-gray-900">Rs.499</span>
         </div>
         <div class="flex mt-5">
-          <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
-          <button class="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Button</button>
-          <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-            </svg>
-          </button>
+          <span class="title-font font-medium text-2xl text-gray-900">TT Rs.1199</span>
+          
         </div>
       </div>
       <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
