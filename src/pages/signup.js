e110a5ccac3a3461203f45d2c9c47695dfeb186d
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 21:52:31 +0500

    All required fileds for signup are done

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -21,7 +21,7 @@ const Signup = () => {
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="#" method="POST">
       <div>
-        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Full Name</label>
         <div class="">
           <input id="name" name="name" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
         </div>
@@ -36,6 +36,15 @@ const Signup = () => {
       <div>
         <div class="flex items-center justify-between">
           <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+         
+        </div>
+        <div class="">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+       <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
          
         </div>
         <div class="">
