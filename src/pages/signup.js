6be08f691e56a57c37eceee649ceaf8824deef5e
Laraib007 <lrbkhan01@gmail.com>
2024-10-23 21:50:36 +0500

    Creating signup fileds

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import img from '../component/Img/Clogo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
 
 const Signup = () => {
   return (
@@ -10,15 +15,21 @@ const Signup = () => {
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
   <Image class="mx-auto mt-0 mb-0 h-90 w-auto" src={img} height={100} width={100}  />
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Signup Now</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="#" method="POST">
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+        <div class="">
+          <input id="name" name="name" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
+        </div>
+      </div>
+      <div>
+        <label for="email" class="block text-sm font-medium  text-gray-900">Email address</label>
+        <div class="">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
         </div>
       </div>
 
@@ -27,7 +38,7 @@ const Signup = () => {
           <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
          
         </div>
-        <div class="mt-2">
+        <div class="">
           <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
         <div class="text-sm text-center">
