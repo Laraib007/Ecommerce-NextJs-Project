44dfa97630c33d9e5b1ccca9b697f82c129293fa
Laraib007 <lrbkhan01@gmail.com>
2024-10-28 23:16:05 +0500

    Alhamdulillah fix all problems

--- middleware.js/mongoose.js
+++ middleware.js/mongoose.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = handler => async (req, res)=>{
-    if(mongoose.connection[0]){
-        console.log(mongoose.connections[0])
+    if(mongoose.connections[0].readyState){
         return handler(req, res)
     }
     await mongoose.connect(process.env.MONGOSSE_URI)
