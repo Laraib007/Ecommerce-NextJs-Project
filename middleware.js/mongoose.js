ca4e05d71a68ec803eb00c9b125fa7a4ae6ef5d1
Laraib007 <lrbkhan01@gmail.com>
2024-10-28 22:33:24 +0500

    creating middleware

---
+++ middleware.js/mongoose.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const connectDB =async (handler)=>{
+    if(mongoose.connection[0].isReadyState){
+        return handler(req, res)
+    }
+    await mongoose.connect(process.env.MONGOSSE_URI)
+    return handler(req, res)
+}
