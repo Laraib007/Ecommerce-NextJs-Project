42505a8241b4bf452f6e23768557cf364ffbfc84
Laraib007 <lrbkhan01@gmail.com>
2024-10-28 22:09:11 +0500

    Exporting UserSchema

--- models/Users.js
+++ models/Users.js
@@ -5,3 +5,4 @@ const UserSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true}
 });
+export default mongoose.model("User", UserSchema)
