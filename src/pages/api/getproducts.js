84749e28a22816cfbadf8906b450447fb4c07828
Laraib007 <lrbkhan01@gmail.com>
2024-11-05 21:54:56 +0500

    fixing all issues

--- src/pages/api/getproducts.js
+++ src/pages/api/getproducts.js
@@ -7,7 +7,12 @@ const handler = async (req, res)=>{
   for(let item in Products){
   
     if(item.title in tshirts){
-
+        if(!tshirts[item.title].color.includes(item.color) && item.avalibleQty > 0){
+          tshirts[item.title].color.push(item.color)
+        }
+        if(!tshirts[item.title].size.includes(item.size) && item.avalibleQty > 0){
+          tshirts[item.title].size.push(item.size)
+        }
     }
     else {
       tshirts[item.title] = JSON.parse(JSON.stringify(item))
