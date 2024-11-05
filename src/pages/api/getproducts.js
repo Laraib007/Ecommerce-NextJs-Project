e7aa7af665a1c6badb57a5cef99042b3b0c3fd5a
Laraib007 <lrbkhan01@gmail.com>
2024-11-05 21:41:19 +0500

    replacing products

--- src/pages/api/getproducts.js
+++ src/pages/api/getproducts.js
@@ -3,6 +3,16 @@ import connectDB from "../../../middleware.js/mongoose";
 
 const handler = async (req, res)=>{
   let product = await Products.find()
+  let tshirts = {}
+  for(let item in Products){
+    if(!tshirts){
+
+    }
+    else {
+      if(tshirts[item.title])
+
+    }
+  }
   res.status(200).json({ product });
 }
 export default connectDB(handler)
