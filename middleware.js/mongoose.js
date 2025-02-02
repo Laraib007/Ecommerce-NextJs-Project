51eddbe03e9d55d08bd11632477cf4c986ed403e
Laraib007 <lrbkhan01@gmail.com>
2025-02-02 10:16:20 -0800

    fixing env

--- middleware.js/mongoose.js
+++ middleware.js/mongoose.js
@@ -4,7 +4,7 @@ const connectDB = handler => async (req, res)=>{
     if(mongoose.connections[0].readyState){
         return handler(req, res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGOSSE_URI)
     return handler(req, res)
 }
 export default connectDB;
