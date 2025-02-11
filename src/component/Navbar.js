18091408a8b06fd488940c47d66a8cef8a28a802
Laraib007 <lrbkhan01@gmail.com>
2025-02-11 09:21:08 -0800

    fixing cart

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -47,7 +47,7 @@ const Navbar = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
       {Object.keys(cart).length == 0 && <div>Your Cart is Empty!</div>}
       {Object.keys(cart).map((k)=>{return <li key={k}>
         <div className='item flex '>
-        <div className='w-2/3 font-semibold '>{cart[k].name}</div>
+        <div className='w-2/3 font-semibold '>{cart[k].name}({cart[k].size}/{cart[k].varient})</div>
         <div className='font-bold flex justify-center items-center  w-1/3 '><FaMinusCircle onClick={()=>removeFromCart(k, 1, cart[k].name, cart[k].price, cart[k].size, cart[k].varient)} 
         
         className='text-pink-600 text-sd mx-1' />{cart[k].qty}<  FaPlusCircle className='text-pink-600 text-sd mx-1' onClick={()=>addToCart(k, cart[k].name, cart[k].price, 1, cart[k].size, cart[k].varient)} />
