70829d355b6f4b66b0817c881548e748675ba072
Laraib007 <lrbkhan01@gmail.com>
2025-02-12 09:21:53 -0800

    populating backend data to frontend

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -83,7 +83,7 @@ const refreshVariants =(newsize, newcolor)=>{
             </a>
           </span>
         </div>
-        <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+        <p class="leading-relaxed">{product.desc}</p>
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div class="flex">
             <span class="mr-3">Color</span>
@@ -114,7 +114,7 @@ const refreshVariants =(newsize, newcolor)=>{
           </div>
         </div> 
         <div class="flex">
-          <span class="title-font font-medium text-2xl text-gray-900">Rs.{}</span>
+          <span class="title-font font-medium text-2xl text-gray-900">Rs.{product.price}</span>
           <button onClick={()=>addToCart(productSlug, product.title, product.price, 1, size, color)} class="flex ml-3 text-white bg-pink-500 border-0 py-2 px-3 focus:outline-none hover:bg-pink-600 rounded"><FaCartShopping className='text-sd mt-1 mr-1 cursor-pointer text-white ' />Add to Cart</button>
           <button class="flex ml-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded"><IoBagCheck className='text-sd mt-1 mr-1 ' />Buy Now</button>
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
