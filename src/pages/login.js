1899b872e10e216fc09179d79ae20de3bcad01a5
Laraib007 <lrbkhan01@gmail.com>
2025-02-14 10:54:38 -0800

    Alhamdulillah fixed login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,21 +19,21 @@ const Login = () => {
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="#" method="POST">
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+        {/* <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label> */}
+        <div>
+          <input placeholder='  Email' id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
       <div>
         <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          {/* <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label> */}
          
         </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+        <div >
+          <input placeholder='  Password' id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
-        <div class="text-sm text-center">
+        <div class="text-sm text-center mt-2">
             <Link href="/forgotpass" class="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</Link>
           </div>
       </div>
