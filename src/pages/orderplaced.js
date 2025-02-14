16ad6f15da3e1b44ef579a3faf939b7b60b6572c
Laraib007 <lrbkhan01@gmail.com>
2025-02-14 10:02:04 -0800

    installing react toastify

--- src/pages/orderplaced.js
+++ src/pages/orderplaced.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+const orderplaced = ({cart, subTotal}) => {
   return (
     <div className='h-3/4 mb-2'>
         <section class="text-gray-600 body-font overflow-hidden ">
