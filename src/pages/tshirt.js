65b293a1d203c5e7a2a0e04e31198ddc65e68103
Laraib007 <lrbkhan01@gmail.com>
2025-02-15 08:20:29 -0800

    fixing product slug

--- src/pages/tshirt.js
+++ src/pages/tshirt.js
@@ -2,8 +2,10 @@ import Link from 'next/link'
 import React from 'react'
 import mongoose from "mongoose";
 import Products from '../../models/Products';
+import { useRouter } from 'next/router';
 
 const Tshirt = ({products}) => {
+  const router = useRouter()
     console.log(products)
   return (
     <div>
@@ -14,11 +16,11 @@ const Tshirt = ({products}) => {
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Welcome to Our Ecommerce Store</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
         </div>
-        <div className="container px-5 py-7 mx-auto">
+        <div  className="container px-5 py-7 mx-auto">
             <div className="flex flex-wrap -m-4 justify-center">
                 
             {Object.keys(products).map((items)=>{ return    <div key={products[items].slug} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
+                    <a onClick={()=>router.push(`/product/${products[items].slug}`) } className=" relative rounded overflow-hidden contents">
                       
           <img alt="ecommerce" className="object-cover object-center w-72 h-72 block " src={products[items].img}/>
                     </a>
