b2bce38dd3bdf2df4b2559ad7469c9fb5aba5153
Laraib007 <lrbkhan01@gmail.com>
2025-02-15 07:31:20 -0800

    now making hoodies page

--- src/pages/hoodies.js
+++ src/pages/hoodies.js
@@ -1,94 +1,88 @@
+import Link from 'next/link'
 import React from 'react'
+import mongoose from "mongoose";
+import Products from '../../models/Products';
 
-const Hoodies = () => {
+const Hoodies = ({products}) => {
+    console.log(products)
   return (
     <div>
-       <section className="text-gray-600 body-font">
+
+    <section className="text-gray-600 body-font">
         <div className="flex py-14 flex-col text-center w-full">
             
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Welcome to Our Ecommerce Store</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
         </div>
         <div className="container px-5 py-7 mx-auto">
-            <div className="flex flex-wrap m-4 justify-center">
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
+            <div className="flex flex-wrap -m-4 justify-center">
+                
+            {Object.keys(products).map((items)=>{ return    <div key={products[items].slug} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
                     <a className=" relative rounded overflow-hidden contents">
                       
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
+          <img alt="ecommerce" className="object-cover object-center w-72 h-72 block " src={products[items].img}/>
                     </a>
                     <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{products[items].title}</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">{products[items].title}</h2>
+                        <p className="mt-1">Rs.{products[items].price}</p>
                     </div>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
+                    <Link href={`/product/${products[items].slug}`}>Buy Now</Link>
+                    <div className=''>
+                        {products[items].size.includes("S") && <span className='border border-s-gray-300 px-1'>S</span>}
+                        {products[items].size.includes("M") && <span className='border border-s-gray-300 px-1 ml-1'>M</span>}
+                        {products[items].size.includes("L") && <span className='border border-s-gray-300 px-1 ml-1'>L</span>}
+                        {products[items].size.includes("XL") && <span className='border border-s-gray-300 px-1 ml-1'>XL</span>}
+                        {products[items].size.includes("XXL") && <span className='border border-s-gray-300 px-1 ml-1'>XXL</span>}
                     </div>
-                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div> <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md m-4  ">
-                    <a className=" relative rounded overflow-hidden contents">
-                      
-          <img alt="ecommerce" className="object-cover object-center h-72 block " src="https://m.media-amazon.com/images/I/9112xNSIlqL._AC_SX522_.jpg"/>
-                    </a>
-                    <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                        <p className="mt-1">$16.00</p>
-                    </div>
-                </div>
+                    <div>
+                    {products[items].color.includes("green") && <button class="border-2 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                    {products[items].color.includes("yellow") && <button class="border-2 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                    {products[items].color.includes("purple") && <button class="border-2 bg-purple-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                    {products[items].color.includes("blue") && <button class="border-2 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                    {products[items].color.includes("red") && <button class="border-2 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                    {products[items].color.includes("black") && <button class="border-2 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
 
+                    </div>
+                </div>
+                })}
+                
+              
+                </div>
             </div>
-        </div>
     </section>
-    </div>
+</div>
   )
 }
+export async function getServerSideProps(products) {
+
+    if(!mongoose.connections[0].readyState){
+    mongoose.connect(process.env.MONGOSSE_URI)
+    }
+    let product = await Products.find({category: "hoodies"})
+    let hood = {}
+  for(let item of product){
+  
+    if(item.title in hood){
+        if(!hood[item.title].color.includes(item.color) && item.avalibleQty > 0){
+          hood[item.title].color.push(item.color)
+        }
+        if(!hood[item.title].size.includes(item.size) && item.avalibleQty > 0){
+          hood[item.title].size.push(item.size)
+        }
+    }
+    else {
+      hood[item.title] = JSON.parse(JSON.stringify(item))
+      if(item.avalibleQty > 0){
+        hood[item.title].color = [item.color]
+        hood[item.title].size = [item.size]
+      }
+
+    }
+  }
+    return {
+      props: {products: JSON.parse(JSON.stringify(hood))},
+    }
+  }
 
 export default Hoodies
