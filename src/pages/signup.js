c59356396d8a8120078d5a5dfb7f1d771751b2b1
Laraib007 <lrbkhan01@gmail.com>
2025-02-16 00:43:20 -0800

    writing code

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,13 +5,45 @@ import Link from 'next/link'
 
 
 
-const Signup = () => {
+const Signup = async () => {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
 
-  let data = {name, email, password}
+  const handleChange = (e)=> {
+if(e.target.name == "name"){
+setName(e.target.value)
 
+}
+if(e.target.name == "email"){
+  setEmail(e.target.value)
+  
+  }
+
+  if(e.target.name == "password"){
+    setPassword(e.target.value)
+    
+    }
+
+  }
+
+
+
+
+
+
+
+  let data = {name, email, password}
+  const response = await fetch('http://localhost:3000/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+  console.log(result);
 
 
 
@@ -31,17 +63,17 @@ const Signup = () => {
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" onSubmit={()=>onFormSubmit} method="POST">
       <div>
         <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Full Name</label>
         <div class="">
-          <input id="name" name="name" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
+          <input onChange={()=>handleChange} value={name} id="name" name="name" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
         </div>
       </div>
       <div>
         <label for="email" class="block text-sm font-medium  text-gray-900">Email address</label>
         <div class="">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
+          <input onChange={()=>handleChange} value={email} id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm "/>
         </div>
       </div>
 
@@ -51,18 +83,10 @@ const Signup = () => {
          
         </div>
         <div class="">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+          <input onChange={()=>handleChange} value={password} id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
-       <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
-         
-        </div>
-        <div class="">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
-        </div>
-      </div>
+      
 
       <div>
         <button type="submit" class="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Sign Up</button>
