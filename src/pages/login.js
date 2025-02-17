f89d0316417779f0661fb35f6571adb9b6218739
Laraib007 <lrbkhan01@gmail.com>
2025-02-17 10:34:02 -0800

    coding

--- src/pages/login.js
+++ src/pages/login.js
@@ -95,11 +95,11 @@ theme="colored"
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form onSubmit={onFormSubmit} class="space-y-6"  method="POST">
       <div>
         {/* <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label> */}
         <div>
-          <input placeholder='  Email' id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+          <input onChange={handleChange} placeholder='  Email' id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
@@ -109,7 +109,7 @@ theme="colored"
          
         </div>
         <div >
-          <input placeholder='  Password' id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+          <input onChange={handleChange} placeholder='  Password' id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
         <div class="text-sm text-center mt-2">
             <Link href="/forgotpass" class="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</Link>
