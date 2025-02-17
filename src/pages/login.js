842261627f8ee0c0f99d4b98bae7e7b276a304ef
Laraib007 <lrbkhan01@gmail.com>
2025-02-17 10:35:08 -0800

    Assign values

--- src/pages/login.js
+++ src/pages/login.js
@@ -99,7 +99,7 @@ theme="colored"
       <div>
         {/* <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label> */}
         <div>
-          <input onChange={handleChange} placeholder='  Email' id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+          <input onChange={handleChange} value={email} placeholder='  Email' id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
@@ -109,7 +109,7 @@ theme="colored"
          
         </div>
         <div >
-          <input onChange={handleChange} placeholder='  Password' id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
+          <input onChange={handleChange} value={password} placeholder='  Password' id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"/>
         </div>
         <div class="text-sm text-center mt-2">
             <Link href="/forgotpass" class="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</Link>
