8195478830580df4849fc5521540da565e506a7b
Laraib007 <lrbkhan01@gmail.com>
2025-02-17 10:28:04 -0800

    Alhamdulillah Fix all bugs glitchs and issues

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -5,18 +5,21 @@ const handler = async (req, res)=>{
     if(req.method == "POST"){
         let u = await Users.findOne({email: req.body.email})
        
-          
-                if(u){
+              if(u){
                     if(req.body.email ==  u.email && req.body.password  == u.password){
                         res.status(200).json({ sucess: "sucess" });
                     }
                     else{
                         res.status(404).json({ warning: "Invalid Creditionals" })
                     }
-                }     else{res.status(404).json({ warning: "User Not Found" })}       
+                } else{
+                    res.status(404).json({ warning: "User Not Found" })
+                }       
             
     
-        }
+        } else {
+            res.status(404).json({ warning: "Invalid Request" })
+        }     
           
         
     } 
