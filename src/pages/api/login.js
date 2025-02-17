985332f9444251243d7006c51dc2b7ef86099bca
Laraib007 <lrbkhan01@gmail.com>
2025-02-17 10:11:14 -0800

    fixing

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,24 +3,25 @@ import connectDB from "../../../middleware.js/mongoose";
 
 const handler = async (req, res)=>{
     if(req.method == "POST"){
-        try {
-            let u =  Users.findOne({email: req.body.email})
+        let u = await Users.findOne({email: req.body.email})
+       
+            try {
                 if(u){
-                    if(email == req.body.email && password == req.boy.password){
+                    if(u.email == req.body.email && u.password == req.body.password){
                         res.status(200).json({ sucess: "sucess" });
                     }
                     else{
                         res.status(404).json({ warning: "Invalid Creditionals" })
                     }
                 }
+            } catch (error) {
+                res.status(404).json({ warning: "User Not Found" })
+            }
+            
+                
             
     
-        } catch (error) {
-            res.status(404).json({ warning: "User Not Found" })
-        }
-       
-    }
-          
+        }         
         
     } 
   
