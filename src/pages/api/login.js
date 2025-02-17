046eaf0e1d493badd6ab351fd1b81155ffd1b8d0
Laraib007 <lrbkhan01@gmail.com>
2025-02-17 10:20:20 -0800

    fixing

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -5,23 +5,19 @@ const handler = async (req, res)=>{
     if(req.method == "POST"){
         let u = await Users.findOne({email: req.body.email})
        
-            try {
+          
                 if(u){
-                    if(u.email == req.body.email && u.password == req.body.password){
+                    if(req.body.email ==  u.email && req.body.password  == u.password){
                         res.status(200).json({ sucess: "sucess" });
                     }
                     else{
                         res.status(404).json({ warning: "Invalid Creditionals" })
                     }
-                }
-            } catch (error) {
-                res.status(404).json({ warning: "User Not Found" })
-            }
-            
-                
+                }            
             
     
-        }         
+        }
+          
         
     } 
   
