f94c8c9909a6a482f2b29795b7a4bd4ee70dd615
Laraib007 <lrbkhan01@gmail.com>
2025-02-18 08:50:53 -0800

    Alhamdulillah fix all issues

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,12 +3,11 @@ import connectDB from "../../../middleware.js/mongoose";
 var CryptoJS = require("crypto-js");
 
 const handler = async (req, res)=>{
-    var bytes  = CryptoJS.AES.decrypt(u.password, 'secret123');
-    var originalText = bytes.toString(CryptoJS.enc.Utf8);
-    console.log(originalText)
     if(req.method == "POST"){
         let u = await Users.findOne({email: req.body.email})
-       
+        var bytes  = CryptoJS.AES.decrypt(u.password, 'secret123');
+        var originalText = bytes.toString(CryptoJS.enc.Utf8);
+        console.log(originalText)
               if(u){
                     if(req.body.email ==  u.email && req.body.password  == originalText){
                         res.status(200).json({ sucess: "sucess" });
