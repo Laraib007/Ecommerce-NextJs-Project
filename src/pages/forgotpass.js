0eaeff96b74be47e37d455f06be81a5a9662ccfd
Laraib007 <lrbkhan01@gmail.com>
2025-02-22 21:21:55 +0500

    redirecting from forget pass page after login

--- src/pages/forgotpass.js
+++ src/pages/forgotpass.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import img from '../component/Img/Clogo.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 const forgotpass = () => {
+  const router = useRouter()
+    
+      useEffect(() => {
+        if(localStorage.getItem('token')){
+          router.push("/")
+        }
+      
+        
+      }, [])
   return (
     <div>
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
