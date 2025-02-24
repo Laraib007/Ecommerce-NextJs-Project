5c6cde3359002315a7e3ee0e8992475512ab40ca
Laraib007 <lrbkhan01@gmail.com>
2025-02-24 22:25:08 +0500

    creating user profile page

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -80,7 +80,7 @@ theme="colored"
      {user.value && <CgProfile onMouseOver={()=>{setDropDown(true)}} onMouseLeave={()=>{setDropDown(false)}} className='text-2xl font-bold cursor-pointer text-pink-600 mr-3' />}
      {dropDown && <div onMouseOver={()=>{setDropDown(true)}} onMouseLeave={()=>{setDropDown(false)}} id="dropdown" class="absolute  right-9 top-4 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-32 dark:bg-pink-600">
     <ul class="py-2  text-sm font-bold text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-      <li className='ml-2 items-center flex row dark:hover:text-gray-300'><IoMdSettings /> <Link  class=" px-1 py-2 dark:hover:text-gray-300" href={'/myAccount'}>
+      <li className='ml-2 items-center flex row dark:hover:text-gray-300'><IoMdSettings /> <Link  class=" px-1 py-2 dark:hover:text-gray-300" href={'/myaccount'}>
        My Account</Link>
       </li>
       <li className='ml-2 items-center flex row dark:hover:text-gray-300'><HiShoppingBag /> <Link  class=" px-1 py-2 dark:hover:text-gray-300" href={'/orders'}>
