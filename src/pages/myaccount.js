cd4c547aefcc5fb7e9fc9387c890fa074164f325
Laraib007 <lrbkhan01@gmail.com>
2025-02-24 22:59:49 +0500

    successfully display popup

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -83,7 +83,7 @@ const Myaccount = () => {
           </div>
         </div>
         <button type="button" data-modal-target="accountInformationModal2" data-modal-toggle="accountInformationModal2" class="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 bg-blue-600 focus:outline-none focus:ring-4 focus:ring-primary-300  dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto">
-          <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
           </svg>
           Edit your data
@@ -92,7 +92,7 @@ const Myaccount = () => {
       
     </div>
     {/* <!-- Account Information Modal --> */}
-    <div id="accountInformationModal2" tabIndex="-1" aria-hidden="true" class="max-h-auto fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden antialiased md:inset-0">
+    <div id="accountInformationModal2" tabIndex="-1" aria-hidden="false" class="max-h-auto fixed left-0 right-0 top-0 z-50  h-[calc(100%-1rem)] max-h-full w-full items-center items-center justify-center overflow-y-auto overflow-x-hidden antialiased md:inset-0">
       <div class="max-h-auto relative max-h-full w-full max-w-lg p-4">
         {/* <!-- Modal content --> */}
         <div class="relative rounded-lg bg-white shadow dark:bg-gray-800">
