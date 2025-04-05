75e5d06df90932421fef603900d293c20a7c360d
Laraib007 <lrbkhan01@gmail.com>
2025-04-05 21:54:34 +0500

    fixing order schema

--- models/Orders.js
+++ models/Orders.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-  email: {type: String, required: true}, 
+  name: {type: String, required: true},  
   product: {type: Object, required: true},
+  email: {type: String, required: true},
+  number: {type: Number, required: true},
+  altNumber: {type: Number, required: true},
   address: {type: String, required: true},
   amount: {type: Number, required: true},
   status: {type: String, default: "pending", required: true}
