acc5e05764b648c760243d95f5880342b691f27f
Laraib007 <lrbkhan01@gmail.com>
2025-04-06 17:40:55 +0500

    upadting checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -36,7 +36,7 @@ const Checkout = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
 
 
      const onFormSubmit = async (e)=>{
-     e.preventDefault()
+    //  e.preventDefault()
        let data = {name, cart, email, number, altNumber, address, subTotal}
        console.log(data)
        let response = await fetch('http://localhost:3000/api/order', {
@@ -102,8 +102,9 @@ const Checkout = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
     <div className=' text-center font-semibold my-2'>Sub Total: <span className='font-bold'> Rs.{subTotal}</span></div>
     </div>
     <div className='flex justify-center px-1'>
+    <Link href={'/order'}>
     <button onClick={onFormSubmit} type="submit" class="flex w-50 text-center  rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Confirm</button>
-   
+    </Link>
     </div>
   </div>
   )
