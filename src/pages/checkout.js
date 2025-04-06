eab46414a928ac20652f77c2e0ce2610368343c5
Laraib007 <lrbkhan01@gmail.com>
2025-04-06 22:00:49 +0500

    fixing issue

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -50,6 +50,7 @@ const Checkout = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
        const result = await response.json();
        console.log(data);
        localStorage.removeItem("cart")
+       clearCart()
        }
   return (
     <div className='container mt-32'>
@@ -102,9 +103,9 @@ const Checkout = ({cart, addToCart, clearCart, removeFromCart, subTotal}) => {
     <div className=' text-center font-semibold my-2'>Sub Total: <span className='font-bold'> Rs.{subTotal}</span></div>
     </div>
     <div className='flex justify-center px-1'>
-    {/* <Link href={'/order'}> */}
+    <Link href={'/orders'}>
     <button onClick={onFormSubmit} type="submit" class="flex w-50 text-center  rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Confirm</button>
-    {/* </Link> */}
+    </Link>
     </div>
   </div>
   
