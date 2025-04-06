815c7140d73db943ca71b7e60b91f1960a00216d
Laraib007 <lrbkhan01@gmail.com>
2025-04-06 21:55:08 +0500

    fixingfixing

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,6 +1,5 @@
 import Orders from "../../../models/Orders";
 import connectDB from "../../../middleware.js/mongoose";
-import { NextResponse } from "next/server";
 
 const handler = async (req, res)=>{
     if(req.method == "POST"){
@@ -16,14 +15,13 @@ const handler = async (req, res)=>{
                 })
                 await o.save()
             res.status(200).json({ sucess: "sucess" });
-            NextResponse.redirect("/")
+            
         } catch (error) {
             res.status(404).json({ warning: "Product not added" })
         }
-       
     }
           
-        
+     
     } 
   
 export default connectDB(handler)
