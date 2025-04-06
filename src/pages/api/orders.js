55b27336f66576ed4a480479aa597fd5f575c477
Laraib007 <lrbkhan01@gmail.com>
2025-04-06 23:13:05 +0500

    verifing jwt token

--- src/pages/api/orders.js
+++ src/pages/api/orders.js
@@ -1,12 +1,13 @@
 import Orders from "../../../models/Orders";
 import connectDB from "../../../middleware.js/mongoose";
-var jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 const handler = async (req, res)=>{
-    let token = req.body.token
-    let data = jwt.verify(token, 'secret123')    
+    if(req.method == "POST"){
+    const token = req.body.token
+    const data = jwt.verify(token, 'secret123')    
     let orders = await Orders.find({email: data.email})
     res.status(200).json(orders)
     } 
-  
+}
 export default connectDB(handler)
