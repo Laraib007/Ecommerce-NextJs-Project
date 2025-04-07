c1b379242af60afedd39f6790dcfee7ae00a5948
Laraib007 <lrbkhan01@gmail.com>
2025-04-07 21:00:55 +0500

    settingup redircting

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -88,13 +88,13 @@ const orders = () => {
                   <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z" />
                   </svg>
-                  Pre-order
+                  {item.status}
                 </dd>
               </dl>
-  
+              <Link href={'/order?id=' + item._id}>
               <div class="w-full grid sm:grid-cols-2 lg:flex lg:w-64 lg:items-center lg:justify-end gap-4">
                   <a href="#" class="w-full inline-flex justify-center rounded-lg  border border-gray-200  px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-black dark:focus:ring-gray-700 lg:w-auto">View details</a>
-              </div>
+              </div></Link>
             </div>})}
           </div>
         </div>
