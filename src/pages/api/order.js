9a20ef2e53c4e4087dec835758ec2c3507e06496
Laraib007 <lrbkhan01@gmail.com>
2025-04-18 21:00:21 +0500

    fixing issue

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -38,8 +38,7 @@ const handler = async (req, res)=>{
                 await o.save()
             res.status(200).json({ sucess: "sucess" });
             for(let slug in cart){
-                product = await Products.findOneAndUpdate({slug: slug}, { $inc:{"avalibleQty": - product[slug].qty}})
-                return product;
+                 await Products.findOneAndUpdate({slug: slug}, { $inc:{"avalibleQty": - product[slug].qty}})
             }
         } catch (error) {
             res.status(404).json({ warning: "Product not added" })
