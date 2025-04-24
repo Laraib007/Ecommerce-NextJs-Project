29e5a926b09c2d81fd91146a5cf5e98e23df47ee
Laraib007 <lrbkhan01@gmail.com>
2025-04-24 23:53:27 +0500

    coding

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -10,7 +10,7 @@ const handler = async (req, res)=>{
             let cart = req.body.cart
             for(let item in cart){
                     sumTotal = cart[item].price * cart[item].qty
-                console.log(cart[item])
+                console.log(item.price)
             product = await Products.findOne({slug: item})
             if(product.avalibleQty < cart[item].qty){
                 return res.status(403).json({"error":product.title + " (" + product.size  + "/" + product.color + ")" + " is out of stock. Please Try Again! Please reduce quantity or remove the product"})
