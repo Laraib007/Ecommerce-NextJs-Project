3fde3ca6058f8cf80c0a38ed159ade1f325d8dd2
Laraib007 <lrbkhan01@gmail.com>
2025-04-25 22:07:08 +0500

    cleaning

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -22,15 +22,7 @@ const Myaccount = () => {
               <dt class="font-semibold text-gray-900 ">Email Address</dt>
               <dd class="text-gray-500 dark:text-gray-400">helene@example.com</dd>
             </dl>
-            <dl>
-              <dt class="font-semibold text-gray-900 ">Home Address</dt>
-              <dd class="flex items-center gap-1 text-gray-500 dark:text-gray-400">
-                <svg class="hidden h-5 w-5 shrink-0 text-gray-400 dark:text-gray-500 lg:inline" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5" />
-                </svg>
-                2 Miles Drive, NJ 071, New York, United States of America
-              </dd>
-            </dl>
+            
             <dl>
               <dt class="font-semibold text-gray-900 ">Delivery Address</dt>
               <dd class="flex items-center gap-1 text-gray-500 dark:text-gray-400">
@@ -47,7 +39,7 @@ const Myaccount = () => {
               <dd class="text-gray-500 dark:text-gray-400">+1234 567 890 / +12 345 678</dd>
             </dl>
             <dl>
-              <dt class="font-semibold text-gray-900 ">Favorite pick-up point</dt>
+              <dt class="font-semibold text-gray-900 ">Nearby point</dt>
               <dd class="flex items-center gap-1 text-gray-500 dark:text-gray-400">
                 <svg class="hidden h-5 w-5 shrink-0 text-gray-400 dark:text-gray-500 lg:inline" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path
