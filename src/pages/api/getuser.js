51fa42a720a93ab08a076e120ee44bd213c81998
Laraib007 <lrbkhan01@gmail.com>
2025-04-27 10:06:48 +0500

    removing console.log

--- src/pages/api/getuser.js
+++ src/pages/api/getuser.js
@@ -7,8 +7,7 @@ const handler = async (req, res)=>{
     if(req.method == "POST"){
         var token = req.body.token
         let user = jwt.verify(token,'topsecret');
-        res.status(200).json({ sucess: "sucess", token});
-        console.log(user)
+        res.status(200).json({ sucess: "sucess", user});
         // let u = await Users.findOne({email: req.body.email})
               
     
