0d3cd1aeeb2322f678e7fd3375616044b59710f4
Laraib007 <lrbkhan01@gmail.com>
2025-04-27 10:06:31 +0500

    updating User schema

--- models/Users.js
+++ models/Users.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: {type: String, required: true}, 
   email: {type: String, required: true, unique: true},
+  city: {type: String, default: ''},
+  address: {type: String,  default: ''},
+  cellNumber: {type: String,  default: ''},
   password: {type: String, required: true}
 });
 mongoose.models = {}
