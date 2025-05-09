f9449958fb9d616649770ee2b14161e094546576
Laraib007 <lrbkhan01@gmail.com>
2025-05-09 20:45:16 +0500

    Setting users page

--- src/pages/admin/users.js
+++ src/pages/admin/users.js
@@ -12,7 +12,7 @@ const Users = () => {
   <div class="container px-5 py-4 mx-auto">
     
     <div class="flex flex-wrap -m-4">
-      <div class="p-4 md:w-1/3 w-full">
+      <div class="p-4 md:w-1/1 w-full">
         <div class="h-full bg-gray-800 text-center bg-opacity-40 p-8 rounded">
          
          <h2 class="title-font font-bold text-2xl text-white">Total Users</h2>
@@ -25,32 +25,7 @@ const Users = () => {
           <h3>--FaishonWears--</h3>
         </div>
       </div>
-      <div class="p-4 md:w-1/3 w-full">
-        <div class="h-full bg-gray-800 text-center bg-opacity-40 p-8 rounded">
-         
-         <h2 class="title-font font-bold text-2xl text-white">Out of Stocks</h2>
-          <a class="inline-flex items-center">
-          
-            <span class="flex-grow flex flex-col pl-4">
-              <span class="inline-flex items-center justify-center w-24 h-24 mr-8 m-4 p-3 ms-3 text-sm font-medium text-red-800 bg-red-100 rounded-full dark:bg-red-900 text-xl dark:text-red-100">3</span>
-            </span>
-          </a>
-          <h3>--FaishonWears--</h3>
-        </div>
-      </div>
-      <div class="p-4 md:w-1/3 w-full">
-        <div class="h-full bg-gray-800 text-center bg-opacity-40 p-8 rounded">
-         
-         <h2 class="title-font font-bold text-2xl text-white">Users In Stocks</h2>
-          <a class="inline-flex items-center">
-          
-            <span class="flex-grow flex flex-col pl-4">
-              <span class="inline-flex items-center justify-center w-24 h-24 mr-8 m-4 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-900 text-xl dark:text-green-100">3</span>
-            </span>
-          </a>
-          <h3>--FaishonWears--</h3>
-        </div>
-      </div>
+     
     </div>
   </div>
 </section><section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
@@ -60,14 +35,14 @@ const Users = () => {
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
                     <form class="flex items-center">
-                        <label for="simple-search" class="sr-only">Search Product ID</label>
+                        <label for="simple-search" class="sr-only">Search User ID</label>
                         <div class="relative w-full">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search Product ID" required=""/>
+                            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search User ID" required=""/>
                         </div>
                     </form>
                 </div>
