5f872eaad351066dae8ba36f4c6b6c462f28edd3
Laraib007 <lrbkhan01@gmail.com>
2025-05-11 12:55:39 +0500

    Shukar Alhamduillah

--- src/pages/api/orders.js
+++ src/pages/api/orders.js
@@ -4,11 +4,12 @@ import jwt from 'jsonwebtoken';
 
 const handler = async (req, res)=>{
     if(req.method == "POST"){
+        let u = await Orders.find()
+        console.log(u.name)
     const token = req.body.token
-    const data = jwt.verify(token, 'topsecret')
-    console.log(Orders.find({ usersInfo: { forAllDBs: true } }))    
+    const data = jwt.verify(token, 'topsecret')  
     let orders = await Orders.find({email: data.email})
-    res.status(200).json(orders)
+    res.status(200).json(u)
     } 
 }
 export default connectDB(handler)
