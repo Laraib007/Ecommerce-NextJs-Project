f9a314eee7ce9c354b913d0f9e0d7aaf0bf14ce4
Laraib007 <lrbkhan01@gmail.com>
2025-05-11 21:43:55 +0500

    CODING

--- src/pages/api/orders.js
+++ src/pages/api/orders.js
@@ -22,20 +22,14 @@ const handler = async (req, res)=>{
                 if(u[i].status.includes("completed")){
                     compOrd += u[i].status.includes("completed")
                     b =  compOrd ;
-                    console.log(b)
                 }
                      }
-                 
-                    
-         
-         res.status(200).json({u: u, a: a})
+         res.status(200).json({u: u, a: a, b: b})
         }
         else{
             let orders = await Orders.find({email: data.email})
             res.status(200).json(orders)
         }
-    
-    
     } 
 }
 export default connectDB(handler)
