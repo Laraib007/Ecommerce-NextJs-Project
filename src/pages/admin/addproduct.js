b2a07125dbd1e18bf4423d1772467c29273235f6
Laraib007 <lrbkhan01@gmail.com>
2025-05-17 23:26:18 +0500

    coding

--- src/pages/admin/addproduct.js
+++ src/pages/admin/addproduct.js
@@ -3,11 +3,29 @@ import Sidebar from './sideBar'
 import cloudinary from '../../../middleware.js/cloudinary'
 const AddProduct = () => {
   const [image, setImage] = useState('')
-const onSubmit = async () =>{
+  const handleChange = async(e)=>{
+ const file = e.target.files[0]
+ if(!file) return
 
- const uploadResult = await cloudinary.uploader.upload(image, {
-  folder: product
+ const data = new FormData()
+
+data.append("file", file)
+data.append("upload_presets", "FashionWear")
+data.append("cloud_name", "dspvvrcfp")
+ await fetch("https://api.cloudinary.com/v1_1/dspvvrcfp/image/upload",{
+  method: "POST",
+  body: data
  })
+
+ setImage(file)
+ console.log(file)
+}
+const onSubmit = async () =>{
+ 
+ const uploadResult = await cloudinary.uploader.upload(image, {
+  folder: "product"
+ })
+ console.log(uploadResult)
 }
 
   return (
@@ -76,10 +94,10 @@ const onSubmit = async () =>{
             </div>
             
 <label class="block mt-2 mb-1 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-xl cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file"/>
+<input onChange={handleChange} name='file' class="block w-full text-sm text-gray-900 border border-gray-300 rounded-xl cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file" type="file"/>
 <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
 </div>
-  <button type="submit" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <button onClick={onSubmit} type="submit" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Product</button>
 </form>
 
     </div>
