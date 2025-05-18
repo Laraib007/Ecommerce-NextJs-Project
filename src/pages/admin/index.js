c738e7efdbacc39c2d9cb34c6fc2793c179fd9b5
Laraib007 <lrbkhan01@gmail.com>
2025-05-18 20:01:09 +0500

    coding

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -11,6 +11,8 @@ const index = () => {
     const [order, setOrder] = useState([])
     const [pendingOrders, setPendingOrders] = useState(0)
     const [completedOrders, setCompletedOrders] = useState(0)
+    const [deleteOrd, setDeleteOrd] = useState(true)
+
       useEffect(() => {
         
       const orderFetch = async ()=>{
@@ -41,35 +43,7 @@ const index = () => {
   return (
     <>
     <Sidebar/>
-    {/* <!-- Modal toggle --> */}
-<div class="flex justify-center m-5">
-    <button id="deleteButton" data-modal-target="deleteModal" data-modal-toggle="deleteModal" class="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="button">
-    Show delete confirmation
-    </button>
-</div>
 
-{/* <!-- Main modal --> */}
-<div id="deleteModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
-    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-        {/* <!-- Modal content --> */}
-        <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-            <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
-            <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this item?</p>
-            <div class="flex justify-center items-center space-x-4">
-                <button data-modal-toggle="deleteModal" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                    No, cancel
-                </button>
-                <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                    Yes, I'm sure
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
     <h1 style={{marginTop: "-4%"}} class=" ml-56 text-3xl font-bold title-font text-black mb-2  text-center">ADMIN DASHBOARD</h1>
     <div class="p-4 sm:ml-64">
        <div class="p-2 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
@@ -214,7 +188,34 @@ const index = () => {
                                 
                                <div className='flex space-x-4 text-xl'>
                                   {item.status != "completed"? <MdModeEditOutline className=' hover:bg-green-500' />: ""} 
-                                  
+                                  {/* <!-- https://medium.com/@mimranisrar6/creating-a-custom-confirmation-dialog-for-an-anchor-tag-4223514a7b0f --> */}
+ {deleteOrd && <div class="fixed inset-0 flex items-center justify-center z-50 backdrop-blur confirm-dialog ">
+    <div class="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
+        <div class=" opacity-25 w-full h-full absolute z-10 inset-0"></div>
+        <div class="bg-white rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
+            <div class="md:flex items-center">
+                <div class="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 flex-shrink-0 mx-auto">
+                <i class="bx bx-error text-3xl">
+                &#9888;
+                </i>
+                </div>
+                <div class="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+                <p class="font-bold">Warning!</p>
+                <p class="text-sm text-gray-700 mt-1">You will lose all of your data by deleting this. This action cannot be undone.
+                </p>
+                </div>
+            </div>
+            <div class="text-center md:text-right mt-4 md:flex md:justify-end">
+                <button id="confirm-delete-btn" class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-200 text-red-700 rounded-lg font-semibold text-sm md:ml-2 md:order-2">
+                    Delete
+                </button>
+                <button id="confirm-cancel-btn" class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-200 rounded-lg font-semibold text-sm mt-4 md:mt-0 md:order-1">
+                Cancel
+                </button>
+            </div>
+        </div>
+    </div>
+</div>}
                                    {item.status != "completed"? <MdDelete  className=' hover:bg-red-500' />: ""} 
                                     <VscGoToSearch  className=' hover:bg-blue-500' />
                               </div>
