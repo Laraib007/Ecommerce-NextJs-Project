066955322e905fd18fa62f09a1cc6e1fb7a468d0
Laraib007 <lrbkhan01@gmail.com>
2025-05-20 22:27:20 +0500

    Alhamdulillh fix all issues

--- src/pages/api/updateorder.js
+++ src/pages/api/updateorder.js
@@ -5,8 +5,7 @@ const handler = async (req, res)=>{
     if(req.method == "POST"){
         const token = req.body.token
         console.log(token)
-        let o = await Orders.updateOne({id: token.id}, {$set: {'status.${completed}': true}})
-console.log(o)
+        let o = await Orders.findOneAndUpdate({id: token}, {status: "completed"})
        await o.save()
        
         res.status(200).json({ "sucess": "sucess" });
