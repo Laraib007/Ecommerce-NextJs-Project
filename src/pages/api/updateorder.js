dd98a16aed9bd4515add30c78798ca496847f87d
Laraib007 <lrbkhan01@gmail.com>
2025-05-20 21:17:20 +0500

    updating product status

---
+++ src/pages/api/updateorder.js
@@ -0,0 +1,17 @@
+import connectDB from "../../../middleware.js/mongoose";
+import Orders from "../../../models/Orders";
+
+const handler = async (req, res)=>{
+    if(req.method == "POST"){
+        const token = req.body.token
+        let o = await Orders.findByIdAndUpdate(token, {status: "completed"})
+
+       await o.save()
+       
+        res.status(200).json({ "sucess": "sucess" });
+    } else {
+        res.status(400).json({err: "bad request"})
+    }
+  
+}
+export default connectDB(handler)
