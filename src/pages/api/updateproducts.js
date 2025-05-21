6ad1b0da8dafb5d2346ba2dd255d7ae5bff4e292
Laraib007 <lrbkhan01@gmail.com>
2025-05-21 19:41:04 +0500

    Bismillah updating update product api

--- src/pages/api/updateproducts.js
+++ src/pages/api/updateproducts.js
@@ -3,10 +3,9 @@ import connectDB from "../../../middleware.js/mongoose";
 
 const handler = async (req, res)=>{
     if(req.method == "POST"){
-        for (let i = 0; i < req.body.length; i++) {
-        let p = await Products.findByIdAndUpdate(req.body[i]._id, req.body[i])
+        let {title, avlQty, price} = req.body
+        let p = await Products.findOneAndUpdate(req.body._id, {title, avlQty, price})
        await p.save()
-    }
        
         res.status(200).json({ "sucess": "sucess" });
     } else {
