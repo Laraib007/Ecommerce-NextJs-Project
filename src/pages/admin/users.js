27926c7fdecd3a6ba032f4eeaf23a82b0ddf73b7
Laraib007 <lrbkhan01@gmail.com>
2025-05-22 00:07:17 +0500

    Creating fixing user page

--- src/pages/admin/users.js
+++ src/pages/admin/users.js
@@ -5,10 +5,13 @@ import Link from 'next/link'
 import { MdModeEditOutline } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 import { VscGoToSearch } from "react-icons/vsc";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Users = () => {
     
         const [users, setUsers] = useState([])
+        const [userdrId, setUserdrId] = useState()
+        const [deleteUser, setDeleteUser] = useState(false)
           useEffect(() => {
             
           const orderFetch = async ()=>{
@@ -31,9 +34,64 @@ const Users = () => {
               orderFetch()
             }
           }, [])
-          
+          const deleteUserFunc = async ( e)=>{
+                                  //   e.preventDefault()
+                                    let token = userdrId
+                                      let data =  {token}
+                                      
+                                      let response = await fetch('http://localhost:3000/api/deleteuser', {
+                                        method: 'POST',
+                                        headers: {
+                                          'Content-Type': 'application/json',
+                                        },
+                                        body: JSON.stringify(data),
+                                      });
+                                      const result = await response.json();
+                                      console.log(result)
+                                  setDeleteUser(false)
+                                  if(result.sucess){
+                                    toast.success('User Deleted Sucessfully', {
+                                      position: "top-left",
+                                  autoClose: 1500,
+                                  hideProgressBar: false,
+                                  closeOnClick: false,
+                                  pauseOnHover: false,
+                                  draggable: true,
+                                  progress: undefined,
+                                  theme: "colored"
+                                      });
+                                      // setTimeout(() => {
+                                      //       router.push("http://localhost:3000/admin")
+                                      //     }, 1500);
+                                        } else { toast.error('Some Error Occurred ', {
+                                          position: "top-left",
+                                          autoClose: 3000,
+                                          hideProgressBar: false,
+                                          closeOnClick: false,
+                                          pauseOnHover: false,
+                                          draggable: true,
+                                          progress: undefined,
+                                          theme: "colored"
+                                          });}
+                                      }
+                                      const toggleDeleteUser =(params) =>{
+                  setUserdrId(params)
+                  setDeleteUser(!deleteUser)
+              }
   return (
     <>
+    <ToastContainer
+    position="top-left"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop
+    closeOnClick={false}
+    rtl={false}
+    pauseOnFocusLoss={false}
+    draggable
+    pauseOnHover={false}
+    theme="colored"
+    />
     <Sidebar/>
     <h1 style={{marginTop: "-4%"}} class=" ml-56 text-3xl font-bold title-font text-black mb-2  text-center">ADMIN DASHBOARD</h1>
     <div class="p-4 sm:ml-64">
@@ -153,9 +211,34 @@ const Users = () => {
                                                    
                                                     
                                                    <div className='flex space-x-4 text-xl'>
-                                                       <MdModeEditOutline className=' hover:bg-green-500' />
-                                                        <MdDelete  className=' hover:bg-red-500' />
-                                                        <VscGoToSearch  className=' hover:bg-blue-500' />
+                                                       <MdDelete onClick={()=>toggleDeleteUser(item._id)} className=' hover:bg-red-500' />
+                                                                                            {deleteUser && <div class="fixed inset-0 flex items-center justify-center z-50 backdrop-blur confirm-dialog ">
+                                                           <div class="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
+                                                               <div class=" opacity-25 w-full h-full absolute z-10 inset-0"></div>
+                                                               <div class="bg-white rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
+                                                                   <div class="md:flex items-center">
+                                                                       <div class="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 flex-shrink-0 mx-auto">
+                                                                       <i class="bx bx-confirm text-3xl">
+                                                                      &#9745;
+                                                                       </i>
+                                                                       </div>
+                                                                       <div class="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+                                                                       <p class="font-bold text-gray-700">Delete Order</p>
+                                                                       <p class="text-sm text-gray-700 mt-1">Are you sure you want to delete this Order?. This action cannot be undone.
+                                                                       </p>
+                                                                       </div>
+                                                                   </div>
+                                                                   <div class="text-center md:text-right mt-4 md:flex md:justify-end">
+                                                                       <button onClick={deleteUserFunc}   id="confirm-delete-btn" class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-green-200 text-green-700 rounded-lg font-semibold text-sm md:ml-2 md:order-2">
+                                                                           Confirmed
+                                                                       </button>
+                                                                       <button onClick={()=>setDeleteUser(false)} id="confirm-cancel-btn" class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-200 rounded-lg font-semibold text-sm mt-4 md:mt-0 md:order-1">
+                                                                       Cancel
+                                                                       </button>
+                                                                   </div>
+                                                               </div>
+                                                           </div>
+                                                       </div>}
                                                   </div>
                                                 </td>
                                             </tr>
