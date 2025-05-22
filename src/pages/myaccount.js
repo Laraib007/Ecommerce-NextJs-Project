71e1260efb21b2ffe926c61cc2eece8975eacf16
Laraib007 <lrbkhan01@gmail.com>
2025-05-22 21:27:16 +0500

    creating updatepassword

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -4,6 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 
 const Myaccount = () => {
     const [hidden, setHidden] = useState(false)
+    const [hiddenPass, setHiddenpass] = useState(false)
       const [name, setName] = useState()
       const [email, setEmail] = useState()
       const [city, setCity] = useState()
@@ -181,6 +182,12 @@ theme="colored"
           </svg>
           Update Your Info
         </button>
+          <button  onClick={()=>setHiddenpass(true) } type="button" data-modal-target="accountInformationModal2" data-modal-toggle="accountInformationModal2" class=" ml-4 inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 bg-blue-600 focus:outline-none focus:ring-4 focus:ring-primary-300  dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto">
+          <svg class="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"></path>
+          </svg>
+          Update Password
+        </button>
       </div>
       
     </div>
@@ -243,8 +250,50 @@ theme="colored"
           </form>
         </div>
       </div>
-    </div>}
+    </div>
+    }
+  {hiddenPass && <div id="accountInformationModal2" tabIndex="-1" aria-hidden="false" class="max-h-auto fixed left-0 right-0 top-0 z-50  h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden antialiased md:inset-0">
+      <div class="max-h-auto my-auto mx-auto relative max-h-full w-full max-w-lg p-4">
+        {/* <!-- Modal content --> */}
+        <div class="relative rounded-lg bg-white shadow dark:bg-gray-800">
+          {/* <!-- Modal header --> */}
+          <div class="flex items-center justify-between rounded-t border-b border-gray-200 p-4 dark:border-gray-700 md:p-5">
+            <h3 class="text-lg font-semibold text-white ">Account Information</h3>
+            <button onClick={()=>setHidden(false)} type="button" class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="accountInformationModal2">
+              <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          {/* <!-- Modal body --> */}
+          <form class="p-4 md:p-5">
+            <div class="mb-5 grid grid-cols-1 gap-4 sm:grid-cols-2">
+             
   
+              <div class="col-span-2 ">
+                <label for="full_name_info_modal" class="mb-2 block text-sm font-medium text-white "> Your Full Name* </label>
+                <input onChange={handleChange} value={name} name='name' type="text" id="full_name_info_modal" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-white focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500" placeholder="Enter your first name" required />
+                <div class="col-span-2 sm:col-span-1">
+                <div class="mb-2 flex items-center gap-2">
+                  <label for="select_city_input_billing_modal" class="block text-sm font-medium text-white "> City* </label>
+                </div>
+                <input onChange={handleChange} value={city} name='city' type="text" id="phone-input" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-white focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500" placeholder="City" />
+              </div>
+              </div>
+              <div class="col-span-2">
+                <label for="pick-up-point-input" class="mb-2 block text-sm font-medium text-white "> Nearby point* </label>
+                <input onChange={handleChange} value={nearby} name='nearby' type="text" id="pick-up-point-input" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-white focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500" placeholder="Enter the pick-up point name" required />
+              </div>
+              
+             
+                </div>
+                
+                
+                </form>
+                </div>
+                </div>
+                </div>}
    
       </div>
   </section>
