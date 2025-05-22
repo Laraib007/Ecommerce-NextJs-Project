e4cf4e73cd02216ae63523459d8bb6a6dac56505
Laraib007 <lrbkhan01@gmail.com>
2025-05-23 00:42:42 +0500

    Alhamdulillah Slider work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-import { Carousel } from 'flowbite';
 import slider1 from "../component/Img/slider1.jpg"
 import slider2 from "../component/Img/slider2.jpg"
 import slider3 from "../component/Img/slider3.jpg"
+import Head from "next/head";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -16,10 +16,24 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const carousel = new Carousel(carouselElement, items, options, instanceOptions);
-  carousel.cycle();
+  var swiper = new Swiper(".progress-slide-carousel", {
+loop: true,
+fraction: true,
+autoplay: {
+  delay: 1200,
+  disableOnInteraction: false,
+},
+pagination: {
+  el: ".progress-slide-carousel .swiper-pagination",
+  type: "progressbar",
+},
+});
   return (
     <>
+    <Head>
+      <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    </Head>
      <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
@@ -28,47 +42,28 @@ export default function Home() {
     </div>
     
 
-<div id="controls-carousel" class="relative w-full" data-carousel="static">
-    {/* <!-- Carousel wrapper --> */}
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
-        <div class=" duration-700 ease-in-out" data-carousel-item>
-            <Image src={slider1} height={100} width={100} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 2 --> */}
-        <div class=" duration-700 ease-in-out" data-carousel-item="active">
-            <Image src={slider2} height={100} width={100} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 3 --> */}
-        <div class=" duration-700 ease-in-out" data-carousel-item>
-            <Image src={slider3} height={100} width={100} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 4 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src="/docs/images/carousel/carousel-4.svg" height={100} width={100} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 5 --> */}
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src="/docs/images/carousel/carousel-5.svg" height={100} width={100} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-    </div>
-    {/* <!-- Slider controls --> */}
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
+{/* <!--HTML CODE--> */}
+<div class="w-full relative">
+<div class="swiper progress-slide-carousel swiper-container relative">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 1 </span>
+  </div>
+</div>
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 2 </span>
+  </div>
+</div>
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 3 </span>
+  </div>
+</div>
+</div>
+<div class="swiper-pagination !bottom-2 !top-auto !w-80 right-0 mx-auto bg-gray-100"></div>
+</div>
 </div>
 
     <div className="flex flex-wrap -m-4">
