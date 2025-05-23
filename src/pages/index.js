33d72cd88829b681b51354277a561047ffc02804
Laraib007 <lrbkhan01@gmail.com>
2025-05-23 20:57:31 +0500

    slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,35 +47,28 @@ pagination: {
 
 
 
-<div id="gallery" class="relative w-full" data-carousel="slide">
-    {/* <!-- Carousel wrapper --> */}
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
-        <div class=" duration-700 ease-in-out" data-carousel-item>
-            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
-        </div>
-        {/* <!-- Item 2 --> */}
-        <div class=" duration-700 ease-in-out" data-carousel-item="active">
-            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
-    </div>
-    </div>
-    {/* <!-- Slider controls --> */}
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
+{/* <!--HTML CODE--> */}
+<div class="w-full relative">
+<div class="swiper progress-slide-carousel swiper-container relative">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 1 </span>
+  </div>
+</div>
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 2 </span>
+  </div>
+</div>
+<div class="swiper-slide">
+  <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+    <span class="text-3xl font-semibold text-indigo-600">Slide 3 </span>
+  </div>
+</div>
+</div>
+<div class="swiper-pagination !bottom-2 !top-auto !w-80 right-0 mx-auto bg-gray-100"></div>
+</div>
 </div>
 
 
