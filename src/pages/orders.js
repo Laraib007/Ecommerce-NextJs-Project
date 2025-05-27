3d892894f9bcf8954455635eb4b269d6832adc07
Laraib007 <lrbkhan01@gmail.com>
2025-05-27 21:17:31 +0500

    coding

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -84,7 +84,7 @@ const orders = () => {
   
               <dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
                 <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Status:</dt>
-                <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-blue-300 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:text-primary-300">
+                <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-blue-300 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:text-black">
                   <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z" />
                   </svg>
@@ -93,7 +93,7 @@ const orders = () => {
               </dl>
               <dl >
                 <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Shipment:</dt>
-                <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-blue-300 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:text-primary-300">
+                <dd class="me-2 mt-1.5 inline-flex items-center rounded bg-blue-300 px-2.5 py-0.5 text-xs font-medium text-primary-600 dark:text-black">
                   <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z" />
                   </svg>
