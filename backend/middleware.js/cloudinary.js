d8e305bde9a2afbb44f39e103af0618ade0114cd
Laraib007 <lrbkhan01@gmail.com>
2025-05-28 14:27:31 +0500

    Fixing code to deploy project on vercel

---
+++ backend/middleware.js/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+
+ cloudinary.config({ 
+        cloud_name: 'dspvvrcfp', 
+        api_key: '939282296698126', 
+        api_secret: 'X0VnBygyCtTJe2kaL8nZqYVzPWI' // Click 'View API Keys' above to copy your API secret
+    });
+
+    export default cloudinary;
